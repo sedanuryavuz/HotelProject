ecb1a1fe09d5e7d218bc9ce525c490a943ce5f9a
Seda Nur Yavuz <sedanuryavz8@gmail.com>
2025-04-14 18:54:17 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/IGenericRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelProject.BusinessLayer.Concrete
+{
+    public interface IGenericRepository<T>
+    {
+    }
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=ApiDb;User ID=sa;Password=12345;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;");
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
